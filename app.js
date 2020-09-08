53d76cb586dcc864b48820e5c8f72a9e2a9676e5
3020 <fitsumcs@gmail.com>
2020-09-08 15:43:40 +0300

    adding the route for api

--- app.js
+++ app.js
@@ -12,6 +12,20 @@ db.on('error', error => console.error(error));
 db.once('open', () => console.log("Databse Connected!!"));
 
 
+// make express use json on body 
+app.use(experss.json());
+
+// root route 
+app.get('/', (req, res) => {
+    res.send("The Api");
+});
+
+// api route 
+const userApi = require('./routes/users');
+
+
+app.use('/api', userApi);
+
 
 
 app.listen(port, () => { console.log("Server has Started!! "); });
---
+++ routes/users.js
@@ -0,0 +1,7 @@
+const experss = require('express');
+
+const router = experss.Router();
+
+
+
+module.exports = router;
