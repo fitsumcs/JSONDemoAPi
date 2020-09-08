e73f9b46f492352f5b3d5034caad58318040bfbf
3020 <fitsumcs@gmail.com>
2020-09-08 16:19:59 +0300

    Add and view route done

--- routes/users.js
+++ routes/users.js
@@ -1,19 +1,46 @@
 const experss = require('express');
+const User = require('../models/user');
 
 const router = experss.Router();
 
 // Getting all users 
-router.get('/users', (req, res) => {
-    res.send("Getting All Users")
+router.get('/users', async(req, res) => {
+    try {
+        const allUsers = await User.find({});
+        res.json(allUsers);
+    } catch (err) {
+
+        res.status(500).json({ error: err.message });
+
+    }
 });
 
 // Getting one user 
 router.get('/users/:id', (req, res) => {
-    res.send(req.params.id)
+    res.send(req.params.id);
 });
 
 // Creating one user 
-router.post('/users', (req, res) => {});
+router.post('/users', async(req, res) => {
+
+    // creating user 
+    const user = new User({
+        name: req.body.name,
+        age: req.body.age,
+        phone: req.body.phone
+    });
+
+    // saving 
+    try {
+        const newUser = await user.save();
+        res.status(201).json(newUser);
+
+    } catch (err) {
+
+        res.status(400).json({ error: err.message });
+    }
+
+});
 
 // Updating one user 
 router.patch('/users/:id', (req, res) => {});
