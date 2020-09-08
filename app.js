7fc19ae68cc5bf1e2cafcd8ca8e9298d026c8495
3020 <fitsumcs@gmail.com>
2020-09-08 15:34:37 +0300

    configure .env

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
+require('dotenv').config({ path: __dirname + '/Env/.env' });
 const experss = require('express');
+const mongoose = require('mongoose');
+
 
 const app = experss();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
+
+mongoose.connect(process.env.LOCAL_DATABSE_URL, { useNewUrlParser: true });
+const db = mongoose.connection;
+db.on('error', error => console.error(error));
+db.once('open', () => console.log("Databse Connected!!"));
+
 
 
 
