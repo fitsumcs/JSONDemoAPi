0b611a26212fdd06d6b82a0dc3430854e2213c9e
3020 <fitsumcs@gmail.com>
2020-09-08 15:50:06 +0300

    Adding the rest route

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,21 @@ const experss = require('express');
 
 const router = experss.Router();
 
+// Getting all users 
+router.get('/users', (req, res) => {
+    res.send("Getting All Users")
+});
 
+// Getting one user 
+router.get('/users/:id', (req, res) => {});
+
+// Creating one user 
+router.post('/users', (req, res) => {});
+
+// Updating one user 
+router.patch('/users/:id', (req, res) => {});
+
+// Creating one user 
+router.delete('/users/:id', (req, res) => {});
 
 module.exports = router;
